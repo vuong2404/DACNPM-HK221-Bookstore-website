7977f4398021ec1b8243c506eb259a726997464d
vuong2404 <minhvuonglht@gmail.com>
2022-10-27 13:17:07 +0700

    Cau hinh thu vien customize-cra, cai dat bable-plugin-module-resolver

---
+++ frontend/src/pages/Admin/Home/Home.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Admin Home page</h1>;
+}
+
+export default Home;
---
+++ frontend/src/pages/Admin/Order/Order.js
@@ -0,0 +1,5 @@
+function Order() {
+    return <h1>Admin Home page</h1>;
+}
+
+export default Order;
