879bf9eb390fb5d60fd9a8ad5cba40e86d2f3d4b
arceusthecreator123 <arceusthecreator123@gmail.com>
2022-11-29 14:18:34 +0700

    almost complete

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,9 @@
 import AdminHomePage from '~/pages/AdminPages/pages/Home';
 import ManageOrder from '~/pages/AdminPages/pages/ManageOrder';
-import ManageBook from '~/pages/AdminPages/pages/ManageBook';
+import ManageBookView from '~/pages/AdminPages/pages/ManageBookView';
+import ManageBookAdvancedSearch from '~/pages/AdminPages/CRUDBook/ManageBookAdvancedSearch';
+import ManageBookAdd from '~/pages/AdminPages/CRUDBook/ManageBookAdd';
+import ManageBookDetail from '~/pages/AdminPages/CRUDBook/ManageBookDetail';
 import OrderDetail from '~/pages/AdminPages/pages/OrderDetails';
 import HomePage from '~/pages/CustomerPages/pages/HomePage';
 import Cart from '~/pages/CustomerPages/pages/CartPages/Cart';
@@ -21,7 +24,19 @@ const routes = [
     },
     {
         path: '/manage-book',
-        component: ManageBook
+        component: ManageBookView
+    },    
+    {
+        path: '/manage-book/add',
+        component: ManageBookAdd
+    },   
+    {
+        path: '/manage-book/advanced-search',
+        component: ManageBookAdvancedSearch
+    },    
+    {
+        path: '/manage-book/details',
+        component: ManageBookDetail
     },
     {
         path: '/manage-order/details',
