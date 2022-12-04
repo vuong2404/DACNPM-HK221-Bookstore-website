4ebf8d87b3c7e7c5bd8f94ffae5a4a03d15ce2b4
Mark Hamaya <MarkQiu@LiangdeMacBook.local>
2022-12-04 16:01:23 +0700

    update new pages

--- client/src/pages/AdminPages/CRUDBook/ManageBookAdd.js
+++ client/src/pages/AdminPages/CRUDBook/ManageBookAdd.js
@@ -22,91 +22,91 @@ const cx = classNames.bind(styles);
 
 
 
-function ManageBookAdd() {
+function ManageBookAdd(){
     return (<DefaultLayout>
         <div className={cx('manage-book-wrapper')}>
             <div className={cx('header')}>
                 <div>
-                    <MyButton
-                        className={cx('unchosen')}
-                        to="/manage-book"
+                <MyButton
+                    className={cx('unchosen')}
+                    to="/manage-book"
                     >
                         Hiển thị
                     </MyButton>
-                    <MyButton
-                        className={cx('chosen')}
-                        to="/manage-book/add"
+                <MyButton
+                    className={cx('chosen')}
+                    to="/manage-book/add"
                     >
                         Thêm sách
                     </MyButton>
-                    <MyButton
-                        className={cx('unchosen')}
-                        to="/manage-book/advanced-search"
+                <MyButton
+                    className={cx('unchosen')}
+                    to="/manage-book/advanced-search"  
                     >
                         Tìm kiếm nâng cao
                     </MyButton>
                 </div>
                 <div className={cx('searchbox')}>
-                    <div class="input-group input-group-lg" >
-                        <span class="input-group-text" id="iconClass">
+                <div class="input-group input-group-lg" >
+                            <span class="input-group-text" id="iconClass">
                             <FontAwesomeIcon icon={faSearch} />
-                        </span>
-                        <input type="text" class="form-control" placeholder="Tên sách/ Tác giả" />
-                    </div>
+                            </span>
+                    <input type="text"  class="form-control" placeholder="Tên sách/ Tác giả"  />
+                </div>
                 </div>
             </div>
             <div className={cx('add-view')}>
-                <form>
-
-                    <div className={cx('add-image')}>
-                        <FontAwesomeIcon icon={faImage} style={{ fontSize: '100px' }} />
-                    </div>
-                    <div className={cx('field')}>
-                        <div className="form-group">
-                            <label>Tên sách:  </label>
-                            <input type="text" className="form-control" />
-                        </div>
-                    </div>
-                    <div className={cx('field')}>
-                        <div className="form-group">
-                            <label>Tác giả: </label>
-                            <input type="text" className="form-control" />
-                        </div>
-                    </div>
-                    <div className={cx('synopsis-field')}>
-                        <div className="form-group">
-                            <label>Sơ lược: </label>
-                            <textarea type="text" className="form-control" rows="5" />
-                        </div>
-                    </div>
-                    <div className={cx('full-field')}>
-                        <div className="form-group">
-                            <label>Thể loại: </label>
-                            <input type="text" className="form-control" />
-                        </div>
-                    </div>
-                    <div className={cx('full-field')}>
-                        <div className="form-group">
-                            <label>Giới hạn: </label>
-                            <input type="text" className="form-control" />
-                        </div>
-                    </div>
-                    <div className={cx('full-field')}>
-                        <div className="form-group">
-                            <label>Số lượng tồn kho: </label>
-                            <input type="text" className="form-control" />
-                        </div>
-                    </div>
-                    <div className={cx('button-field')}>
-                        <input type="submit" value="Thêm sách" className="btn btn-primary btn-lg w-25" />
-                    </div>
-                </form>
-            </div>
+            <form>
+                
+                <div className={cx('add-image')}>
+                    <FontAwesomeIcon icon={faImage} style={{fontSize:'100px'}} />
+                </div>
+                <div className={cx('field')}>
+                <div className="form-group">
+                    <label>Tên sách:  </label>
+                    <input type="text" className="form-control"/>
+                </div>
+                </div>
+                <div className={cx('field')}>
+                <div className="form-group">
+                    <label>Tác giả: </label>
+                    <input type="text" className="form-control"/>
+                </div>
+                </div>
+                <div className={cx('synopsis-field')}>
+                <div className="form-group">
+                    <label>Sơ lược: </label>
+                    <textarea type="text" className="form-control" rows="5"/>
+                </div>
+                </div>
+                <div className={cx('full-field')}>
+                <div className="form-group">
+                    <label>Thể loại: </label>
+                    <input type="text" className="form-control"/>
+                </div>
+                </div>
+                <div className={cx('full-field')}>
+                <div className="form-group">
+                    <label>Giới hạn: </label>
+                    <input type="text" className="form-control"/>
+                </div>
+                </div>
+                <div className={cx('full-field')}>
+                <div className="form-group">
+                    <label>Số lượng tồn kho: </label>
+                    <input type="text" className="form-control"/>
+                </div>
+                </div>
+                <div className={cx('button-field')}>
+                    <input type="submit" value="Thêm sách" className="btn btn-primary btn-lg w-25" />
+                </div>
+            </form>
+        </div>
         </div>
     </DefaultLayout>
-    )
-
-
+)
+        
+    
 }
 
 export default ManageBookAdd;
--- client/src/pages/AdminPages/pages/ManageBookView.js
+++ client/src/pages/AdminPages/pages/ManageBookView.js
@@ -30,53 +30,53 @@ const cx = classNames.bind(styles);
 
 const products = [
     {
-        id: 1,
+        id:1,
         title: "Harry Potter 1",
         author: "J.K.Rowling",
         synopsis: "Ten-year-old Harry Potter is an orphan who lives in the fictional London suburb of Little Whinging, Surrey, with the Dursleys: his uncaring Aunt Petunia, loathsome Uncle Vernon, and spoiled cousin Dudley.",
-        tags: ["Huyền ảo", "Kịch tính"],
-        time: "2022-20-10 00:00:00",
-        amount: 10
+        tags: ["Huyền ảo","Kịch tính"],
+        time:"2022-20-10 00:00:00",
+        amount:10
     },
     {
-        id: 2,
+        id:2,
         title: "Harry Potter 2",
         author: "J.K.Rowling",
         synopsis: "The story follows Harry's tumultuous second year at Hogwarts School of Witchcraft and Wizardry, including an encounter with Voldemort, the wizard who killed Harry's parents.",
-        tags: ["Huyền ảo", "Kịch tính"],
-        time: "2022-20-10 00:00:00",
-        amount: 10
+        tags: ["Huyền ảo","Kịch tính"],
+        time:"2022-20-10 00:00:00",
+        amount:10
     },
     {
-        id: 3,
+        id:3,
         title: "S.A.O Progressive 1",
         author: "Kawahara Reki",
         synopsis: "The volume contains Kirito and Asuna's adventures on the 1st and 2nd floors of Aincrad, including two officially published side stories from Reki's web novel.",
         tags: ["Tiểu thuyết Nhật", "Huyền ảo", "Khoa học viễn tưởng", "Hành động"],
-        time: "2022-20-10 00:00:00",
-        amount: 10
+        time:"2022-20-10 00:00:00",
+        amount:10
     },
     {
-        id: 4,
+        id:4,
         title: "S.A.O Progressive 2",
         author: "Kawahara Reki",
         synopsis: "This volume covers the events that took place while clearing the 3rd Floor of Aincrad.",
         tags: ["Tiểu thuyết Nhật", "Huyền ảo", "Khoa học viễn tưởng", "Hành động"],
-        time: "2022-20-10 00:00:00",
-        amount: 10
+        time:"2022-20-10 00:00:00",
+        amount:10
     },
     {
-        id: 5,
+        id:5,
         title: "S.A.O Progressive 3",
         author: "Kawahara Reki",
         synopsis: "This volume covers the events that took place while clearing the 4th Floor of Aincrad.",
         tags: ["Tiểu thuyết Nhật", "Huyền ảo", "Khoa học viễn tưởng", "Hành động"],
-        time: "2022-20-10 00:00:00",
-        amount: 10
+        time:"2022-20-10 00:00:00",
+        amount:10
     },
 ];
 
-function ManageBookView() {
+function ManageBookView(){
     let pages = 10; // = orderList.length ;
     const initState = {
         currentPage: 1,
@@ -84,40 +84,40 @@ function ManageBookView() {
         listOrder: products,
     };
     const [state, dispatch] = useReducer(reducer, initState);
-    return (
+    return(
         <DefaultLayout>
             <div className={cx('manage-book-wrapper')}>
                 <div className={cx('header')}>
                     <div>
-                        <MyButton
-                            className={cx('chosen')}
-                            to="/manage-book">
+                    <MyButton
+                        className={cx('chosen')}
+                        to="/manage-book">
                             Hiển thị
                         </MyButton>
-                        <MyButton
-                            className={cx('unchosen')}
-                            to="./add"
+                    <MyButton
+                        className={cx('unchosen')}
+                        to="./add"
                         >
                             Thêm sách
                         </MyButton>
-                        <MyButton
-                            className={cx('unchosen')}
-                            to="./advanced-search"
+                    <MyButton
+                        className={cx('unchosen')}
+                        to="./advanced-search"  
                         >
                             Tìm kiếm nâng cao
                         </MyButton>
                     </div>
                     <div className={cx('searchbox')}>
-                        <div class="input-group input-group-lg" >
-                            <span class="input-group-text" id="iconClass">
+                    <div class="input-group input-group-lg" >
+                                <span class="input-group-text" id="iconClass">
                                 <FontAwesomeIcon icon={faSearch} />
-                            </span>
-                            <input type="text" class="form-control" placeholder="Tên sách/ Tác giả" />
-                        </div>
+                                </span>
+                        <input type="text"  class="form-control" placeholder="Tên sách/ Tác giả"  />
+                    </div>
                     </div>
                 </div>
                 <div >
-                    <Table bordered hover className={cx('tableview')}>
+                <Table bordered hover className={cx('tableview')}>
                         <thead>
                             <tr>
                                 <th className="text-center">Tên sách</th>
@@ -142,19 +142,19 @@ function ManageBookView() {
                                                     <span>{tagItem}</span>
                                                 </div>))
                                         }
-                                    </td>
+                                        </td>
                                     <td className="text-center">{item.time}</td>
                                     <td className="text-center">{item.amount}</td>
                                     <td className="text-center">
-                                        <MyButton class="btn btn-outline-primary"
-                                            to={`details/${item.id}`}
-                                        >
-                                            <FontAwesomeIcon icon={faEdit} />
-                                        </MyButton>
-                                        <MyButton class="btn btn-outline-danger">
-                                            <FontAwesomeIcon icon={faTrash} />
-                                        </MyButton>
-                                    </td>
+                                    <MyButton class="btn btn-outline-primary"
+                                     to={`details/${item.id}`}
+                                    >    
+                                        <FontAwesomeIcon icon={faEdit} />
+                                    </MyButton>
+                                    <MyButton class="btn btn-outline-danger">
+                                        <FontAwesomeIcon icon={faTrash} />
+                                    </MyButton>
+                                         </td>
                                 </tr>
                             </tbody>
                         ))}
