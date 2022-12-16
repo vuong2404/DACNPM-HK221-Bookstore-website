003d5b67ef555526b4d085cb78008725fa1a6690
arceusthecreator123 <arceusthecreator123@gmail.com>
2022-12-17 03:25:27 +0700

    maybe final

--- client/src/pages/AdminPages/CRUDBook/ManageBookAdvancedSearch.js
+++ client/src/pages/AdminPages/CRUDBook/ManageBookAdvancedSearch.js
@@ -5,6 +5,10 @@ import DefaultLayout from '../DefaultLayout';
 import classNames from 'classnames/bind';
 import styles from '../pages/AdminPage.module.scss';
 import MyButton from '~/components/Button';
+import React from 'react';
+import axios from 'axios';
+import { getBookById, getBookLists, postBook, searchBooks } from '~/api/bookApi';
+import { json } from 'react-router-dom';
 const cx = classNames.bind(styles);
 
 //fetch API
@@ -21,6 +25,26 @@ const cx = classNames.bind(styles);
 
 
 function ManageBookAdd(){
+    const [state, setState] = React.useState({
+        title: "",
+        author: "",
+        publisher:"",
+        categoryId:"",
+      })
+      const handleSubmit = async() => {
+        // store the states in the form data
+        axios.get(`http://localhost:8080/api/book/`,state).then((res)=>res.data)
+        console.log(state);
+      }
+      const handleChange = (event) => {
+        const value = event.target.value;
+        setState({
+          ...state,
+          [event.target.name]: value
+        });
+      }
+
+
     return (<DefaultLayout>
         <div className={cx('manage-book-wrapper')}>
             <div className={cx('header')}>
@@ -44,47 +68,40 @@ function ManageBookAdd(){
                         Tìm kiếm nâng cao
                     </MyButton>
                 </div>
-                <div className={cx('searchbox')}>
-                <div class="input-group input-group-lg" >
-                            <span class="input-group-text" id="iconClass">
-                            <FontAwesomeIcon icon={faSearch} />
-                            </span>
-                    <input type="text"  class="form-control" placeholder="Tên sách/ Tác giả"  />
-                </div>
-                </div>
             </div>
             <div className={cx('add-view')}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 
                 <div className={cx('full-field')}>
                 <div className="form-group">
                     <label>Tên sách:  </label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" name="title"  className="form-control" defaulValue={state.title} onChange={handleChange}/>
                 </div>
                 </div>
                 <div className={cx('full-field')}>
                 <div className="form-group">
                     <label>Tác giả: </label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" name="author"  className="form-control" defaulValue={state.author} onChange={handleChange}/>
                 </div>
                 </div>
                 <div className={cx('full-field')}>
                 <div className="form-group">
                     <label>Thể loại: </label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" name="categoryId"  className="form-control" defaulValue={state.categoryId} onChange={handleChange}/>
                 </div>
                 </div>
                 <div className={cx('full-field')}>
                 <div className="form-group">
-                    <label>Giới hạn: </label>
-                    <input type="text" className="form-control"/>
+                    <label>Nhà xuất bản: </label>
+                    <input type="text"name="publisher" className="form-control" defaulValue={state.publisher} onChange={handleChange}/>
                 </div>
                 </div>
                 <div className={cx('button-field')}>
-                    <MyButton className="btn btn-primary btn-lg w-25"  to="/manage-book">
-                        Tìm kiếm
-                        </MyButton>
+
+                    <input type="submit" value="Tìm kiếm" className="btn btn-primary btn-lg w-25" />
+                    
                 </div>
+                <code>{JSON.stringify(state)}</code>
             </form>
         </div>
         </div>
--- server/controllers/book.controllers.js
+++ server/controllers/book.controllers.js
@@ -2,6 +2,7 @@ const Book = require("../models/book.models");
 var model = new Book();
 
 exports.getList = async (req, res) => {
+  console.log("ihi");
   let params = req.query ;
   model.getAll(params, (err, data) => {
     if (err) {
@@ -21,6 +22,17 @@ exports.getBook = async (req, res) => {
   });
 };
 
+exports.searchBooks = async (req, res) => {
+  console.log("haha");
+  model.getBooks(req.body, (err, data) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+};
+
 exports.addBook = async (req, res) => {
   model.create(req.body, (err, data) => {
     if (err) {
@@ -33,6 +45,7 @@ exports.addBook = async (req, res) => {
 
 exports.updateBook = async (req, res) => {
   let id = req.params.id;
+  console.log(req.body);
   model.update(id, req.body, (err, data) => {
     if (err) {
       res.status(400).send(err);
